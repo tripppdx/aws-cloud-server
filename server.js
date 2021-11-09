b2e696680e5058c345f88c68e5ac6e7625f7569a
Tripp Lucas <tripppdx@gmail.com>
2021-11-08 18:11:11 -0800

    initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+"use strict";
+
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(express.static("./public"));
+app.listen(PORT, () => {
+  console.log("App is running");
+});
